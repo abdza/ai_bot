5425903a0b55550fc50a08e0e47c5fe4d796a033
abdza <abdullah.zainul@gmail.com>
2023-04-08 14:39:13 +0800

    Added ability to reset chat and will now separate memory by chat id

--- chatter.py
+++ chatter.py
@@ -6,6 +6,7 @@ import logging
 import settings
 import openai
 import textract
+import sqlite3
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 from pydub import AudioSegment
@@ -22,18 +23,36 @@ script_path = os.path.abspath(__file__)
 # Get the directory containing the current script
 script_dir = os.path.dirname(script_path)
 
-messages = []
+con = sqlite3.connect(os.path.join(script_dir,"chat.db"))
 
-def get_response(content):
+# messages = []
+
+def update_db():
+    cursor = con.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS chat (id INTEGER PRIMARY KEY, timestamp, role TEXT, user TEXT, chat TEXT, message TEXT)")
+    con.commit()
+    cursor.close()
+
+def get_response(content,update):
     try:
-        messages.append({'role':'user','content':content})
+        cursor = con.cursor()
+        cursor.execute("INSERT INTO chat (timestamp, role, user, chat, message) VALUES (datetime('now'), 'user', ?, ?, ?)", (update.message.from_user.id, update.message.chat.id, content))
+        con.commit()
+        messages = cursor.execute("SELECT message,role FROM chat WHERE chat = ? ORDER BY timestamp", (update.message.chat.id,)).fetchall()
+        history = [{'role':'assistant','content':m[0]} if m[1]=='assistant' else {'role':'user','content':m[0]} for m in messages]
+        print('history:',history)
         response = openai.ChatCompletion.create(
             model='gpt-3.5-turbo',
-            messages=messages
+            messages=history
         )
-        messages.append(response.choices[0].message)
+        print(response)
+        cursor.execute("INSERT INTO chat (timestamp, role, user, chat, message) VALUES (datetime('now'), 'assistant', ?, ?, ?)", (update.message.from_user.id, update.message.chat.id, response.choices[0].message.content))
+        print("after saving this response:",response.choices[0].message)
+        con.commit()
+        cursor.close()
         return response.choices[0].message.content
     except Exception as e:
+        print("Error: ", e)
         raise e
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -65,7 +84,7 @@ async def document_processing(update: Update, context: ContextTypes.DEFAULT_TYPE
             new_file.write(downloaded_file)
         filetext = textract.process(os.path.join(doc_dir,filename))
         usermsg = str(update.message.caption) + "\nFile content: " + str(filetext).replace('\n\n','\n')
-        response = get_response(usermsg)
+        response = get_response(usermsg,update)
         await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
     except Exception as e:
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, " + str(e))
@@ -82,7 +101,7 @@ async def voice_processing(update: Update, context: ContextTypes.DEFAULT_TYPE):
         ogg_audio = AudioSegment.from_file(os.path.join(script_dir,'voices',filename + '.ogg'), format="ogg")
         ogg_audio.export(os.path.join(script_dir,'voices',filename + '.mp3'), format="mp3")
         transcript = openai.Audio.transcribe("whisper-1", open(os.path.join(script_dir,'voices',filename + '.mp3'),'rb'))
-        response = get_response(transcript.text)
+        response = get_response(transcript.text,update)
         await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
     except Exception as e:
         print("error:",e)
@@ -95,21 +114,34 @@ async def catch_all(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print("Mtest:",mentiontest)
     if update.message.chat.type == ChatType.PRIVATE or any(mentiontest):
         try:
-            response = get_response(update.message.text)
+            response = get_response(update.message.text,update)
             await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
         except Exception as e:
             await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, " + str(e))
     else:
         pass
 
+async def reset(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        cursor = con.cursor()
+        cursor.execute("delete from chat where chat=?", (update.message.chat.id,))
+        con.commit()
+        cursor.close()
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Reset done")
+    except Exception as e:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, " + str(e))
+
 if __name__ == '__main__':
+    update_db()
     application = ApplicationBuilder().token(settings.bot_key).build()
     start_handler = CommandHandler('start', start)
+    reset_handler = CommandHandler('reset', reset)
     imagine_handler = CommandHandler('imagine', imagine)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), catch_all)
     voice_handler = MessageHandler(filters.VOICE, voice_processing)
     doc_handler = MessageHandler(filters.Document.ALL, document_processing)
     application.add_handler(imagine_handler)
+    application.add_handler(reset_handler)
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
     application.add_handler(voice_handler)
